644e2f5ea2a4996da1e035cb2ef10e2eafba07bb
DELL <DELL@DESKTOP-GI6341T>
2025-01-26 02:07:47 +0530

    TL --Branch2 enhancement

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
@@ -6,4 +6,12 @@ public class NetBankingService {
 	public String openAccount(String addhar,double initialamount,String type) {
 		return"account is opened with acc number::"+new Random().nextLong(500000000000L);
 	}
+	
+	public String withdraw(long acno,double amount) {
+		return amount+"is withdrow from the account number::"+acno;
+	}
+	
+	public String deposit(long acno,double amount) {
+		return amount+"is deposit from the account number::"+acno;
+	}
 }
