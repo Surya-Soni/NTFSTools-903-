36f0571849ae82a66624fa26aa4be06786a252bc
DELL <DELL@DESKTOP-GI6341T>
2025-01-27 10:37:54 +0530

    commit

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
@@ -3,7 +3,7 @@ package com.nt.netbanking;
 import java.util.Random;
 
 public class NetBankingService {
-	public String openAccount(String addhar,double initialamount,String type) {
+	public String openAccount (String addhar,double initialamount,String type) {
 		return"account is opened with acc number::"+new Random().nextLong(500000000000L);
 	}
 	
@@ -12,10 +12,10 @@ public class NetBankingService {
 		return "Account is closed";
 	}
 	public String withdraw(long acno,double amount) {
-		return amount+"is withdrow from the account number::"+acno;
+		return amount +"is withdrow from the account number::"+acno;
 	}
 	
-	public String deposit(long acno,double amount) {
+	public String deposit (long acno,double amount) {
 		return amount+"is deposit from the account number::"+acno;
 
 	}
